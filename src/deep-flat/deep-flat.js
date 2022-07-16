48db1469b9c9b9366eb795deeca08fdf6406e642
Kiishi Akande <kiishy.akande@gmail.com>
2022-07-16 22:59:30 +0200

    fixing linting issues

--- src/deep-flat/deep-flat.js
+++ src/deep-flat/deep-flat.js
@@ -4,7 +4,7 @@
  * It returns a new array and there are no side-effects.
  *
  * @param {Array} [array=[]] - The array to flatten.
- * @param depth - the depth of the nesting.
+ * @param depth - The depth of the nesting.
  * @returns {Array} Returns the new flattened array.
  * @example
  *
